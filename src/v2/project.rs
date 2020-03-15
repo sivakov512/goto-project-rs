1fac1ba65bce132dbb6e0a1044428b09c741d7e4
Nikita Sivakov <sivakov512@gmail.com>
2020-03-15 17:45:21 +0300

    Add ability to get opening command of project with subdir

--- src/v2/project.rs
+++ src/v2/project.rs
@@ -1,6 +1,7 @@
 #![allow(dead_code)]
 use serde_derive::Deserialize;
 use std::env;
+use std::path::{Path, PathBuf};
 
 #[derive(Deserialize)]
 pub struct Project {
@@ -10,8 +11,13 @@ pub struct Project {
 }
 
 impl Project {
-    fn opening_command(&self) -> String {
-        let mut command_parts: Vec<String> = vec![format!("cd {}", self.path)];
+    fn opening_command(&self, subdir: Option<&str>) -> String {
+        let path = match subdir {
+            Some(subdir) => Path::new(&self.path).join(subdir),
+            None => PathBuf::from(&self.path),
+        };
+
+        let mut command_parts: Vec<String> = vec![format!("cd {}", path.to_str().unwrap())];
         command_parts.extend_from_slice(&self.instructions);
         command_parts.extend_from_slice(&[env::var("SHELL").unwrap(), "clear".to_owned()]);
 
@@ -22,7 +28,6 @@ impl Project {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::env;
 
     #[test]
     fn returns_expected_for_project_without_instructions() {
@@ -32,12 +37,31 @@ mod tests {
             instructions: vec![],
         };
 
-        let got = project.opening_command();
+        let got = project.opening_command(None);
 
         assert_eq!(
             got,
             format!(
                 "cd /tmp/goto/example/ && {} && clear",
+                env::var("SHELL").unwrap()
+            )
+        )
+    }
+
+    #[test]
+    fn returns_expected_for_project_without_instructions_but_with_subdir() {
+        let project = Project {
+            name: "Example".to_owned(),
+            path: "/tmp/goto/example/".to_owned(),
+            instructions: vec![],
+        };
+
+        let got = project.opening_command(Some("subsub"));
+
+        assert_eq!(
+            got,
+            format!(
+                "cd /tmp/goto/example/subsub && {} && clear",
                 env::var("SHELL").unwrap()
             )
         )
@@ -51,12 +75,31 @@ mod tests {
             instructions: vec!["call_something".to_owned(), "source /tmp/stuff".to_owned()],
         };
 
-        let got = project.opening_command();
+        let got = project.opening_command(None);
 
         assert_eq!(
             got,
             format!(
                 "cd /tmp/goto/example/ && call_something && source /tmp/stuff && {} && clear",
+                env::var("SHELL").unwrap()
+            )
+        )
+    }
+
+    #[test]
+    fn returns_expected_for_project_with_instructions_and_subdir() {
+        let project = Project {
+            name: "Example".to_owned(),
+            path: "/tmp/goto/example/".to_owned(),
+            instructions: vec!["call_something".to_owned(), "source /tmp/stuff".to_owned()],
+        };
+
+        let got = project.opening_command(Some("subsub"));
+
+        assert_eq!(
+            got,
+            format!(
+                "cd /tmp/goto/example/subsub && call_something && source /tmp/stuff && {} && clear",
                 env::var("SHELL").unwrap()
             )
         )
